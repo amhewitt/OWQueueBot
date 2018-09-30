393ae0a6731252ac94e414f34b8860aa2a4ff010
Adam Hewitt <10320357+amhewitt@users.noreply.github.com>
2018-09-30 19:10:46 -0400

    add delimiter support for queue command

--- commands/queue.js
+++ commands/queue.js
@@ -1,7 +1,5 @@
 const Player = require("../models/player.js");
 const mongoose = require("mongoose");
-
-/* declared up here because blizzard likes to change thresholds >_> */
 const GMThreshold = 350;
 const MasterThreshold = 500;
 const Threshold = 1000;
@@ -12,10 +10,10 @@ exports.run = async (client, message, args) => {
     });
     const authorId = message.author.id;
     const authorServerId = message.guild.id;
+   
     
     if(!args[0]) {
         
-        let match = {};
         let userSr;
         let lowerBound, upperBound;
         
@@ -77,56 +75,122 @@ exports.run = async (client, message, args) => {
         
     } else {
         
-        let otherBtag = args[0];
-        // btags must be at least 3 characters long
-        // thus the earliest a # can appear is at index 3
-        if (otherBtag.indexOf("#") <= 2) return message.reply("that's not a battletag!");
-        let userSr, otherSr;
+        if(isNaN(args[0])){
+
+            let otherBtag = args[0];
+            // btags must be at least 3 characters long
+            // thus the earliest a # can appear is at index 3
+            if (otherBtag.indexOf("#") <= 2) return message.reply("that's not a battletag!");
+            let userSr, otherSr;
         
-        console.log("Searching for userid " + authorId.toString() + " and guild " + authorServerId.toString() + ".");
-        Player.findOne( {userId: authorId, serverId: authorServerId} , 
-                       (err, player) => {
-            if (err) {
-                console.error(err);
-                return message.reply("I could not read the database!");
-            }
+            console.log("Searching for userid " + authorId.toString() + " and guild " + authorServerId.toString() + ".");
+            Player.findOne( {userId: authorId, serverId: authorServerId} , 
+                           (err, player) => {
+                if (err) {
+                    console.error(err);
+                    return message.reply("I could not read the database!");
+                }
         
-            if (!player) {
-                return message.reply("You're not in the database. Please run \`o!init\` or \`o!help init\` before using this command.");
-            } else {
-                btag = player.battleNet;
-                if (btag === otherBtag) return message.reply("you can obviously queue with yourself!");
+                if (!player) {
+                    return message.reply("You're not in the database. Please run \`o!init\` or \`o!help init\` before using this command.");
+                } else {
+                    btag = player.battleNet;
+                    if (btag === otherBtag) return message.reply("you can obviously queue with yourself!");
                 
-                userSr = player.skillRating;
+                    userSr = player.skillRating;
                 
-                console.log("Searching for matches for " + otherBtag.toString() + " in guild " + authorServerId.toString() + ".");
-                Player.findOne( {battleNet: otherBtag, serverId: authorServerId}, 
-                               (err, otherPlayer) => {
-                    const compareFail = "you and " + otherBtag + " are too far in SR!";
-                    const compareSuccess = "you and " + otherBtag + " are able to queue together!"
+                    console.log("Searching for matches for " + otherBtag.toString() + " in guild " + authorServerId.toString() + ".");
+                    Player.findOne( {battleNet: otherBtag, serverId: authorServerId}, 
+                                   (err, otherPlayer) => {
+                        const compareFail = "you and " + otherBtag + " are too far in SR!";
+                        const compareSuccess = "you and " + otherBtag + " are able to queue together!"
                     
+                        if (err) {
+                            console.error(err);
+                            return message.reply("I could not read the database!")
+                        }
+                    
+                        if (!otherPlayer) {
+                            return message.reply("that player isn't in the database!")
+                        } else {
+                            otherSr = otherPlater.skillRating;
+                        
+                            if (userSr >= 4000 || otherSr >= 4000) {
+                                return abs(userSr - otherSr) > GMThreshold ? message.reply(compareFail) : message.reply(compareSuccess);
+                            } else if (userSr >= 3500 | otherSr >= 3500) {
+                                return abs(userSr - otherSr) > MasterThreshold ? message.reply(compareFail) : message.reply(compareSuccess);
+                            } else {
+                                 return abs(userSr - otherSr) > Threshold ? message.reply(compareFail) : message.reply(compareSuccess);
+                            }
+                        }
+                    });
+                }
+            
+            });
+
+        } else {
+            const delimiter = args[0];
+            let _GMThreshold = Math.min(delimiter, GMThreshold);
+            let _MasterThreshold = Math.min(delimiter, MasterThreshold);
+            let _Threshold = Math.min(delimiter, Threshold);
+
+            console.log("Searching for userid " + authorId.toString() + " in guild " + authorServerId.toString() + " with custom delimiter " + Math.min(delimiter, Threshold).toString() + ".");
+            Player.findOne( {userId: authorId , serverId: authorServerId} , 
+                            (err, player) => {
+                if (err) {
+                    console.error(err);
+                    return message.reply("I could not read the database!");
+                }
+        
+                if (!player) {
+                    return message.reply("You're not in the database. Please run \`o!init\` or \`o!help init\` before using this command.");
+                } else {
+                    userSr = player.skillRating;
+                
+                    // BIG IF ENERGY
+                
+                if (userSr < (3500 - _Threshold)) {
+                    upperBound = userSr + _Threshold;
+                    lowerBound = userSr - _Threshold;
+                } else if (userSr < (3500 - _MasterThreshold)) {
+                    upperBound = 3499;
+                    lowerBound = userSr - _Threshold;
+                } else if (userSr < 3500) {
+                    upperBound = userSr + _MasterThreshold;
+                    lowerBound = userSr - _Threshold;
+                } else if (userSr < (4000 - _GMThreshold)) {
+                    upperBound = 3999;
+                    lowerBound = userSr - _MasterThreshold;
+                } else if (userSr < 4000) {
+                    upperBound = userSr + _GMThreshold;
+                    lowerBound = userSr - _MasterThreshold;
+                } else {
+                    upperBound = userSr + _GMThreshold;
+                    lowerBound = userSr - _GMThreshold;
+                }
+                
+                console.log("Searching for matches in " + authorServerId.toString() + " between " + lowerBound.toString() + " and " + upperBound.toString() + ".");
+                Player.find( { skillRating: { $gte: lowerBound, $lte: upperBound } , userId : { $ne: authorId } , serverId: authorServerId }, (err, players) => {
                     if (err) {
                         console.error(err);
-                        return message.reply("I could not read the database!")
+                        return message.reply("I could not read the database!");
                     }
                     
-                    if (!otherPlayer) {
-                        return message.reply("that player isn't in the database!")
+                    if (!players[0]) {
+                        return message.reply("I could not find anyone in the database that you can queue with!");
                     } else {
-                        otherSr = otherPlater.skillRating;
-                        
-                        if (userSr >= 4000 || otherSr >= 4000) {
-                            return abs(userSr - otherSr) > GMThreshold ? message.reply(compareFail) : message.reply(compareSuccess);
-                        } else if (userSr >= 3500 | otherSr >= 3500) {
-                            return abs(userSr - otherSr) > MasterThreshold ? message.reply(compareFail) : message.reply(compareSuccess);
-                        } else {
-                            return abs(userSr - otherSr) > Threshold ? message.reply(compareFail) : message.reply(compareSuccess);
+                        let output = ("I found the following players you can queue with:\n");
+                        for (let pl of players) {
+                            output += pl.battleNet + ": " + pl.skillRating.toString() + " SR\n";
                         }
+                        return message.reply(output);
                     }
                 });
             }
-            
         });
+
+
+        }
     }
 };
 
