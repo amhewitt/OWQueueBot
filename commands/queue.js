36f350d18aeff1e55cad03269d1895e6a1f6979b
Adam Hewitt <adam.hewitt73@gmail.com>
2018-09-30 22:57:25 -0400

    removed some debug logging

--- commands/queue.js
+++ commands/queue.js
@@ -30,8 +30,6 @@ exports.run = async (client, message, args) => {
             } else {
                 userSr = player.skillRating;
                 
-                // BIG IF ENERGY
-                
                 if (userSr < (3500 - Threshold)) {
                     upperBound = userSr + Threshold;
                     lowerBound = userSr - Threshold;
@@ -146,11 +144,7 @@ exports.run = async (client, message, args) => {
                     return message.reply("You're not in the database. Please run \`o!init\` or \`o!help init\` before using this command.");
                 } else {
                     userSr = player.skillRating;
-                console.log("GM Threshold: " + _GMThreshold.toString());
-                console.log("Master Threshold: " + _MasterThreshold.toString());
-                console.log("Threshold: " + _Threshold.toString());
-                    // BIG IF ENERGY
-                
+               
                 if (userSr < (3500 - _Threshold)) {
                     upperBound = userSr + _Threshold;
                     lowerBound = userSr - _Threshold;
